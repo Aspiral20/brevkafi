5249532634a4a44bb747c509f9857279bffa8e57
Victor <palanciucvictor20@gmail.com>
2022-05-23 10:00:50 +0300

    My First Commit

---
+++ slick/main.js
@@ -0,0 +1,7 @@
+$('document').ready(() => {
+    let rightCarousel = $('main .main__container .right-bar .right-bar__top .right-carousel');
+    $('.right-carousel').slick({
+        arrows: false,
+        dots: true
+    })
+});
